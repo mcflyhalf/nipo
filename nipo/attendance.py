9587ef0553c1f08505af17e0ccd536ebc74fbf9b
Josiah <mcflyhalf@live.com>
2019-03-11 12:42:04 +0400

    Create attendance as a list. Next thing to do is to serialise and persist this attendance record

--- nipo/attendance.py
+++ nipo/attendance.py
@@ -7,6 +7,12 @@
 
 #The nipo calendar is here(https://calendar.google.com/calendar/b/1?cid=ODBmc2g3ajllMjg5amYybGJuYThmODk2dDBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ). It is a Google calendar where when you invite it to any events it is ging to accept given that this event doesnt clash with anything else on its calendar. This will be the default calendar that'll be used for timetabling in this project.
 
+from nipo import get_logger, test_session 
+from nipo.db.schema import Student, Course, Module
+
+
+
+logger = get_logger("nipo_attendance")
 
 # This class (MarkAttendance) will have functions related to marking attendance. The core function of this class will be to take as input a student id, a module and a date. The class would then mark that student present for that class. The intended strategy to use is to have a multidimensional Python list (to mimic a matrix or excel sheet) where the columns are all the class dates and the rows are the student attendances. Each cell (intersection between row r and column c) is then a binary value representing whether the student r attended the class on the date c.
 # This class should also be able to serialise and deserialise this list in order to persist it in the DB as a blob.
@@ -16,7 +22,7 @@
 # In future, maybe consider moving to a non relational DB
 class MarkAttendance:
 	def __init__(self, studentid, modulecode, moduledate):
-		'''Create a single attendance record'''
+		'''Create a single attendance record for a student in a module'''
 		pass
 
 	def markpresent(self,timearrived):
@@ -33,19 +39,42 @@ class MarkAttendance:
 
 class ModuleAttendance:
 	'''A class that manipulates the structure of any module's attendance record (Think of this like creating an attendance sheet for the module and adding students who are registered to this module into that sheet)'''
-	def __init__(self, modulecode):
-		#Check if the module code exists, si it doesnt, throw an exception.
+	def __init__(self, modulecode, session=test_session):
+		#Check if the module code exists, if it doesnt, throw an exception.
 		#Then check whether the attendance record exists for this module, if not then create it. Otherwise, do nothing
-		self.modulecode = modulecode
-		pass
+		self.modulecode = str(modulecode)
+		self.session=session
 
+		self.module = session.query(Module).\
+							  filter(Module.code == self.modulecode).\
+							  one_or_none()
+
+		if not self.module:
+			raise ValueError("The module code >>{}<< does not exist".format(self.modulecode))
+
+		if not self.module.attendance:
+			logger.info("Creating and persisting attendance record for module >>{}<<".format(self.module.name))
+			attendance_record = self.createattendance()
+			logger.debug("Created attendance record for module >>{}<<".format(self.module.name))
+			self.persistattendance(attendance_record, test_session)
+			logger.debug("Persisted attendance record for module >>{}<<".format(self.module.name))
+			logger.info("Created and persisted attendance record for module >>{}<<".format(self.module.name))
+
+	#This function breaks if a module is attached to more than one course e.g if maths2 is done by both TIE and EMT. To be fixed in version 3
 	def createattendance(self):
 		#Create the first column of the attendance record for this module. The first column is a list of student ID's for all the students registered for this module
 		#Make sure to log this action
-		pass
+		modulecourse = self.module.course_code
+		modulestudents = self.session.query(Student).filter(Student.course_uid == modulecourse)
+		attendance_list = []
+		attendance_list.append("Student ID's")
+		for student in modulestudents:
+			attendance_list.append(student.id)
+		
+		return attendance_list
 
-	def persistattendance(self,attendancerecord,engine):
-		#Pickle the attendance record and persist it to the appropriate table in the engine
+	def persistattendance(self,attendancerecord,session):
+		#Pickle the attendance record and persist it to the appropriate table in the db using the session provided
 		#Make sure to log this action
 		pass
 
@@ -61,5 +90,5 @@ class StudentAttendance:
 
 		pass
 
-	def get_attendance:
+	def get_attendance(self):
 		pass
