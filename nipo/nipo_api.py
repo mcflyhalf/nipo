7ddb3a3c66ab2c40396996f8309be2a18adabe1a
Josiah <mcflyhalf@live.com>
2019-05-28 17:25:40 +0400

    Create and test preliminary api endpoints that work using curl and return json data. Still need to implement a way to pretty print most of the information. Also need to decide what happens in case a get request is sent to many of these endpoints. This is unless I can create a concept of users/logins

--- nipo/nipo_api.py
+++ nipo/nipo_api.py
@@ -1,7 +1,7 @@
-#import Flask and create api functions here
+import datetime
 from nipo.attendance import ModuleAttendance
 from nipo import attendance, production_session, test_session, db
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify, redirect, url_for
 
 session = test_session	#Change to production_session in the production environment. This would then utilise production data(bases) 
 
@@ -36,9 +36,10 @@ def get_attendance_student(studentID):
 
 
 def get_attendance_student_module(studentID,modulecode):
-	unpickled_attendance = get_attendance_module(modulecode)
 
-	return attendance.get_student_attendance(studentID, unpickled_attendance)
+	stud = attendance.StudentAttendance(studentID, session)
+	return stud.get_module_attendance(modulecode)
+
 
 def update_attendance_student_module(studentID, modulecode, sessiondate, present=False):
 	#Check for exceptions thrown in case of non-existent student and invalid session date. then catch them as appropriate
@@ -91,12 +92,11 @@ def landing():
 under_cons_msg = "OOPS! This part of the website is still under construction"
 
 # Return a list of the modules that a student is taking
-@app.route('/modules')
-def mishmash1():
-	return under_cons_msg
+@app.route('/modules', methods = ['GET','POST'])
+def get_student_modules():
+	return get_student_attendance()
 
-#Not working at the moment
-# Return attendance record for the student logged in
+# Return attendance record for the student logged in for the specified module
 @app.route('/module/attendance', methods = ['GET','POST'])
 def get_student_module_attendance():
 	if request.method == 'POST':
@@ -104,8 +104,14 @@ def get_student_module_attendance():
 		modulecode = request.form.get('modulecode')
 
 		student_attendance = get_attendance_student_module(studentID,modulecode)
+		resp = dict()
+		resp['Student ID'] = studentID
+		resp['Module Code'] = "Module Name not yet Implemented"	#To Do
+		resp['Student Name'] = "student Name not yet implemented"	#To Do
+		resp['Module Name'] = modulecode 
+		resp['Student attendance'] = student_attendance
 
-		return student_attendance 	#TODO: Have this returned by a pretty template
+		return jsonify(resp) 	#TODO: Have this returned by a pretty template
 	return under_cons_msg + 'for a Get request'
 
 # Return the modules a student is enrolled in with summary attendance (Dashboard)
@@ -118,35 +124,73 @@ def get_student_attendance():	#For all modules
 
 		student_attendance = dict()
 
+		#TODO: Include student name and ID in this response
+
 		for module in student_modules:
 			student_attendance[module.code] = student.get_module_attendance(module.code)
 
-		return str(student_attendance) 	#TODO: Have this returned by a pretty  dashboard-like template
+		return jsonify(student_attendance) 	#TODO: Have this returned by a pretty  dashboard-like template
 	return under_cons_msg + 'for a Get request'
 
 
 # Return attendance for a student for a module 
 @app.route('/student/module', methods = ['GET','POST'])
-def mishmash4():	#Not Yet implemented
-	if request.method == 'POST':
-		studentID = request.form.get('studentID')
-		modulecode = request.form.get('modulecode')
-
-		student_attendance = get_attendance_student_module(studentID,modulecode)
-
-		return student_attendance 	#TODO: Have this returned by a pretty template
-	return under_cons_msg + 'for a Get request'
+def get_module_attendance_student():	#Not Yet implemented
+	return get_student_module_attendance()#TODO: Have this returned by a pretty template
 
 #Return the list of students in a module
 @app.route('/students', methods = ['POST'])
-def mishmash5():		#Not Yet implemented
+def get_module_students():		#Not Yet implemented
+	if request.method == 'POST':
+		modulecode = request.form.get('modulecode')
+		module = attendance.ModuleAttendance(modulecode, session)
+
+		mod_students = module.students
+
+		resp = dict()
+
+		for student in mod_students:
+			resp[student.id] = dict()
+			resp[student.id]['Name'] = student.name
+			#resp[student.id]['Attendance'] = 
+
+		#student_attendance = get_attendance_student_module(studentID,modulecode)
+
+		return jsonify(resp) 	#TODO: Have this returned by a pretty template
+	return under_cons_msg + 'for a Get request'
+
+# Return attendance record for the student logged in for the specified module
+@app.route('/module/attendance/mark', methods = ['GET','POST'])
+def set_student_module_attendance():
 	if request.method == 'POST':
 		studentID = request.form.get('studentID')
 		modulecode = request.form.get('modulecode')
+		status = request.form.get('status')
+		sess_date = request.form.get('SessionDate')	#Expects YYYY-MM-DDTHH:MM Format (Iso 8601)
+		try:
+			studentID = int(studentID)
+		except ValueError:
+			return "The student ID {} must be a number (integer)".format(studentID)
+		try:
+			sess_date = datetime.datetime.strptime(sess_date, '%Y-%m-%dT%H:%M')
+		except ValueError:
+			return "The date {} is not in the required YYYY-MM-DDTHH:mm format".format(sess_date) 
 
-		student_attendance = get_attendance_student_module(studentID,modulecode)
+		if status.lower() == "present":
+			status = True
+		else:
+			status = False
 
-		return student_attendance 	#TODO: Have this returned by a pretty template
+		mod_attendance = attendance.ModuleAttendance(modulecode, session)
+
+		mod_attendance.updateAttendance(studentID,sess_date, present=status)
+		try:
+			mod_attendance.updateAttendance(studentID,sess_date, present=status)			
+		except ValueError:
+			return "The date >>{}<< does not have a class session for the module with code >>{}<<".format(sess_date.isoformat(), modulecode)
+
+		#redirect to show student's attendance for the module. Preserve the POST parameters
+		return redirect(url_for('get_student_module_attendance'), code=307)
 	return under_cons_msg + 'for a Get request'
 
 
