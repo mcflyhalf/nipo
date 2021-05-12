9b8ba5b9e07a62ede04a01d45eea8be62c4f476d
mcflyhalf <mcflyhalf@live.com>
2021-05-12 09:46:17 +0400

    Fix get_logger to work when no existing logging directory exists

--- nipo/conf.py
+++ nipo/conf.py
@@ -23,6 +23,8 @@ def get_logger(loggerName):
 	# To atttempt compression of old log files, try https://docs.python.org/3/howto/logging-cookbook.html#using-a-rotator-and-namer-to-customize-log-rotation-processing
 	config = get_configs()
 	log_location = os.path.join(config['Install Location'],'log','nipo.log')
+	if not os.path.exists(log_location):
+		os.makedirs(log_location[:len('nipo.log')*-1])
 	fh = RotatingFileHandler(log_location, mode='a', maxBytes=1*1024*1024,backupCount=10, encoding=None, delay=0)
 	fh.setLevel(logging.DEBUG)
 	# Console handler that logs warnings or higher
