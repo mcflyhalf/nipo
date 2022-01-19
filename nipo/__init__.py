a5f820d0c3ae478cca66353eeae37d0426523a72
mcflyhalf <mcflyhalf@live.com>
2022-01-19 23:24:36 +0400

    Fix bootstrapping code, update pip invocation in CI file to match currrent pip version

--- nipo/__init__.py
+++ nipo/__init__.py
@@ -1,7 +1,7 @@
-from flask import g
-from nipo.conf import Session #import may need to be moved just before line 6
-
 def get_db_session():
+# Strange import location is to allow bootstrapping. Need a better solution
+    from flask import g
+    from nipo.conf import Session #import may need to be moved just before line 6
     if 'db_session' not in g:
         g.db_session = Session()
 
