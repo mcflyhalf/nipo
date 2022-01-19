e820370f53f2e23a45b56f66d3f2e323b871ef96
mcflyhalf <mcflyhalf@live.com>
2022-01-20 01:54:40 +0400

    Bump version and sort out some circular imports

--- nipo/__init__.py
+++ nipo/__init__.py
@@ -1,7 +1,7 @@
 def get_db_session():
 # Strange import location is to allow bootstrapping. Need a better solution
     from flask import g
-    from nipo.conf import Session #import may need to be moved just before line 6
+    from nipo.conf import Session
     if 'db_session' not in g:
         g.db_session = Session()
 
