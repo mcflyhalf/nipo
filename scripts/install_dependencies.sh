4d194e9298cb25e56fdae2be04952c47147c6a67
mcflyhalf <mcflyhalf@live.com>
2022-02-02 22:21:52 +0400

    Explicitly ensure you are in correct directory

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # pip install upgradeable from reqs.txt
+cd /home/ubuntu/projects/nipo 
 /home/ubuntu/projects/nipo/bin/python -m pip install --upgrade -r requirements.txt
 
 # pip install nipo (not editable)
-cd /home/ubuntu/projects/nipo 
 /home/ubuntu/projects/nipo/bin/python -m pip install .
