86ed1d3f4750c79ee7433fb68ffbfff9da29960f
mcflyhalf <mcflyhalf@live.com>
2022-02-02 23:29:54 +0400

    Make env vars visible. Attempt 3

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # pip install upgradeable from reqs.txt
 cd /home/ubuntu/projects/nipo 
-source /nipo_prod_env.sh
+source nipo_prod_env.sh
 /home/ubuntu/projects/nipo/bin/python -m pip install --upgrade -r requirements.txt
 
 # pip install nipo (not editable)
