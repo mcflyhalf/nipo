e8cb978472967790eec5f2bbcf010f98678e39c7
mcflyhalf <mcflyhalf@live.com>
2022-02-02 19:22:45 +0400

    Prep for AWS codeDeploy

---
+++ scripts/start_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# start services
+service nipo start
+service nipo_celery start
---
+++ scripts/stop_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Stop services (nipo/gunicorn, nipo_celery)
+service nipo stop
+service nipo_celery stop
